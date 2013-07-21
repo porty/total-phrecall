72a3abe4471d90f26d146648f08829c78611e733
porty <pppppppman@gmail.com>
2013-07-21 11:50:48 +1000

    More testing in example.php

--- example.php
+++ example.php
@@ -12,6 +12,8 @@
 		<title>Total Recall PHP + Facebook Example</title>
 		<link href="css/main.css" rel="stylesheet" type="text/css"/>
 		<script src="js/jquery-2.0.3.min.js" type="text/javascript"></script>
+		<script src="js/friends.js.php" type="text/javascript"></script>
+		<script src="js/game.js" type="text/javascript"></script>
 	</head>
 	<body>
 		<div class="deck">
@@ -20,9 +22,9 @@
 	$i = 0;
 	foreach ($friends as $friend)
 	{
-
+		$y = $i % 6;
 ?>
-			<div class="card" id="card_<?php echo $i; ?>" style="background-image: url('<?php echo $friend->getImageUrl(); ?>'); background-size: cover;" onclick="document.getElementById('tts_<?php echo $i; ?>').play();">
+			<div class="card" id="card_<?php echo $i; ?>" style="background-image: url('<?php echo $friend->getImageUrl(); ?>');" onclick="document.getElementById('tts_<?php echo $i; ?>').play();">
 				<audio id="tts_<?php echo $i; ?>" preload="auto" src="http://tts-api.com/tts.mp3?q=<?php echo $friend->getName(); ?>"/>
 			</div>
 <?php
